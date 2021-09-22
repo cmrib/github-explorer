cf289d9fdf2db889351858f23f01f5b812e0a8ba
cmrib <ciceromirandar@gmail.com>
2021-09-22 10:43:36 -0300

    Estado do componente

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -2,9 +2,9 @@ export function RepositoryItem(props) {
 
     return (
         <li>
-            <strong>{props.repository?.name ?? 'Default'}</strong>
+            <strong>{props.repository.name ?? 'Default'}</strong>
             <p>{props.repository.description}</p>
-            <a href={props.repository.link}> Acessar repositórios </a>
+            <a href={props.repository.link} target="_blank"> Acessar repositórios </a>
         </li>
     );
 }
